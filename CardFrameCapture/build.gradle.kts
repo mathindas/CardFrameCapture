f90da666b09eef962e62fe0aceb983cfc9aa5d3b
Rivaldo Rendy Mathindas <mathindas.r@gmail.com>
2023-11-15 22:52:40 +0700

    add maven publish

--- CardFrameCapture/build.gradle.kts
+++ CardFrameCapture/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    `maven-publish`
 }
 
 android {
@@ -28,10 +29,15 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+tasks {
+    "publishToMavenLocal" {
+        dependsOn("assemble")
+    }
 }
